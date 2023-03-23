009e2846b3a28a6cfb5178b33a21f84e162ef85c
17jhagan <jacobhagan98@gmail.com>
2023-03-23 12:48:56 -0700

    Adding voltage_to_kelvin function

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 # Imports
 import os
+import math
 
 # Known SDI-12 device addresses
 # TODO: Maybe this should go in a config file!
@@ -9,7 +10,7 @@ SDI_12_DEVICES = {
     'z' : '100K-THERMISTOR'
 }
 
-HEADER = "DateTime,Hostname,Sensor1,VWC(m^3),Temp(°C),EC(dS/m),Sensor2,WaterPotential(kPa),Temp(°C),Sensor3,Voltage(V)\n"
+HEADER = "DateTime,Hostname,Sensor1,VWC(m^3),Temp(°C),EC(dS/m),Sensor2,WaterPotential(kPa),Temp(°C),Sensor3,Voltage(V),Temp(K)\n"
 
 def format_output(data_str):
     # Convert data string into list and trim off the last 7 irrelevant data points
@@ -22,6 +23,9 @@ def format_output(data_str):
                 # If a known SDI-12 address matches with an address in the data, 
                 # overwrite the address with a common sensor name
                 csv_data[index] = SDI_12_DEVICES[csv_data[index]]
+    
+    temp_kelvin = voltage_to_kelvin(float(csv_data[-1]))
+    csv_data.append("%0.2f" % temp_kelvin)
 
     # Return newly formatted data as a string
     return ",".join(csv_data)
@@ -31,6 +35,7 @@ def setup_csv(filepath):
     file_exists = os.path.exists(filepath)
 
     if(file_exists):
+        # Open existing file
         file = open(filepath, 'a')
         
     else:
@@ -41,4 +46,11 @@ def setup_csv(filepath):
     return file
 
 def voltage_to_kelvin(volts):
-    pass
+    resistance = 100000 * (volts / (5-volts))
+    R0 = 100000
+    # B value from KOKISO 100k thermistor sales page
+    B = 3950
+
+    temp_kelvin = 1 / ( (1 / 298) + (1 / B) * ( math.log(resistance / R0) ) )
+    
+    return temp_kelvin
