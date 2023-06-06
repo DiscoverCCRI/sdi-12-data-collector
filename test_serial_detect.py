263e042caa28545d04217f7d2fc5d3df7a642793
17jhagan <jacobhagan98@gmail.com>
2023-06-06 11:03:06 -0700

    Adding break

--- test_serial_detect.py
+++ test_serial_detect.py
@@ -11,11 +11,12 @@ for port in available_ports:
     #sern=sern[:-1] if ((type(port.vid) is int) and (port.vid==VID_FTDI) and (platform.system()=='Windows')) else sern # Windows OS FTDI driver adds 'A' to the end of the serial number.
     #print('%d)\t%s\t(USB VID=%04X)\t Serial#:=%s' % (i, port.device, vidn, sern))
     if port.vid == VID_FTDI:
-        print("Found SDI-12 USB Adapter")
+        print("[+] Found SDI-12 USB Adapter")
         sdi_12_adapter = port.device
         sdi_12_adapter_sern = port.serial_number
+        break
 
     else:
-        print("No match found")
+        print("[-] No match found")
 
 print(f"Found the SDI-12 USB Adapter Serial #: {sdi_12_adapter_sern} at {sdi_12_adapter}")
