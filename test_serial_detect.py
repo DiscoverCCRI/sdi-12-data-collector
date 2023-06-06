6c8b443a1bc2d351dff71c6c76f5df29b644fe0e
17jhagan <jacobhagan98@gmail.com>
2023-06-06 10:19:59 -0700

    Fixed iterable

--- test_serial_detect.py
+++ test_serial_detect.py
@@ -5,10 +5,10 @@ VID_FTDI = 0x0403;
 available_ports = serial.tools.list_ports.comports()
 print('\nDetected the following serial ports:')
 i=0
-for w in a:
-    vidn=w.vid if (type(w.vid) is int) else 0
-    sern=w.serial_number if (type(w.serial_number) is str) else 'NONE'
-    sern=sern[:-1] if ((type(w.vid) is int) and (w.vid==VID_FTDI) and (platform.system()=='Windows')) else sern # Windows OS FTDI driver adds 'A' to the end of the serial number.
-    print('%d)\t%s\t(USB VID=%04X)\t Serial#:=%s' % (i, w.device, vidn, sern))
+for port in available_ports:
+    vidn=port.vid if (type(port.vid) is int) else 0
+    sern=port.serial_number if (type(port.serial_number) is str) else 'NONE'
+    sern=sern[:-1] if ((type(port.vid) is int) and (port.vid==VID_FTDI) and (platform.system()=='Windows')) else sern # Windows OS FTDI driver adds 'A' to the end of the serial number.
+    print('%d)\t%s\t(USB VID=%04X)\t Serial#:=%s' % (i, port.device, vidn, sern))
     i=i+1
 total_ports = i  # now i= total ports
