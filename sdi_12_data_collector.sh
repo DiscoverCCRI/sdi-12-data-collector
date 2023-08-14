ce04adcaaa5adb3a941b110cae55fe203d1070f9
Michael Chestnut <72172361+MichaelChestnut@users.noreply.github.com>
2023-08-14 15:48:09 -0700

    Update sdi_12_data_collector.sh

--- sdi_12_data_collector.sh
+++ sdi_12_data_collector.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-# NOTE: These paths could be cleaned up if we set up a 'working directory' in the cronjob.
-/usr/bin/python3 /SOME/PATH/TO/sdi_12_data_collector.py >> /SOME/PATH/TO/logs/sdi_12_data_collector-`date '+%Y%m%d'`.log 2>&1
+python3 /sdi_12_data_collector.py >> /Data/logs/sdi_12_data_collector-`date '+%Y%m%d'`.log 2>&1
