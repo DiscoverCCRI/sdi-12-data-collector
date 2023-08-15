4579668b55098b110eb10fc702c2dd4d2a682903
Michael Chestnut <72172361+MichaelChestnut@users.noreply.github.com>
2023-08-14 18:54:15 -0700

    Update sdi_12_data_collector.sh

--- sdi_12_data_collector.sh
+++ sdi_12_data_collector.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python3 /sdi_12_data_collector.py >> /Data/logs/sdi_12_data_collector-`date '+%Y%m%d'`.log 2>&1
+python3 /sdi_12_data_collector.py >> /Data/logs/sdi-12-`date '+%Y%m%d'`.log 2>&1
